937626de1f4306337c2ba32a07fff2f4a41101a0
Thomas Jager <thomasj@thomasjager.ca>
2018-11-09 12:12:11 -0500

    Added implementation

---
+++ StringStream.h
@@ -0,0 +1,21 @@
+#ifndef INCLUDE_STRINGSTREAM_H
+#define INCLUDE_STRINGSTREAM_H
+
+#include <Arduino.h>
+#include <Stream.h>
+
+class StringStream : public Stream {
+public:
+    size_t write(uint8_t c) { buff += static_cast<char>(c); return 1; }
+    int available() { return 0; }
+    int read() { return EOF; }
+    int peek() { return EOF; }
+    void flush() {}
+    StringStream& operator=(const String& other) { buffer = other; return *this; }
+    String str() { return buff; }
+
+private:
+    String buff;
+};
+
+#endif
